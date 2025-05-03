05cdbdb11d2eff14b812e92de84d2bc1aec856ee
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-05-03 10:54:08 +0300

    adding inter font and metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,20 @@
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 import { routing } from "@/i18n/routing";
 
 import "../globals.css";
 import TopBar from "@/components/TopBar";
 import { LocaleProvider } from "@/contexts/LocaleContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Digital Library JO",
+  description: "An e-commerce application based in Jordan",
+};
 
 export default async function LocaleLayout({
   children,
@@ -21,7 +31,7 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body>
+      <body className={`${inter.className} antialiased`}>
         <NextIntlClientProvider>
           <LocaleProvider locale={locale}>
             <TopBar />
