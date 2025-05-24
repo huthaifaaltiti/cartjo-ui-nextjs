fb167e999fd5341f8da3909f6026933120a0b931
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-05-24 11:50:25 +0300

    editing styling

--- src/components/admin/DashboardSideNav.tsx
+++ src/components/admin/DashboardSideNav.tsx
@@ -13,7 +13,7 @@ const DashboardSideNav = () => {
     <aside className="h-full w-full p-4">
       <ul className="flex flex-col gap-8">
         {/* Logo */}
-        <li className="p-2 bg-gray-800 rounded">
+        <li className="p-2 bg-primary-950 rounded">
           <Link href={`/${locale}/dashboard`}>
             <Logo />
           </Link>
