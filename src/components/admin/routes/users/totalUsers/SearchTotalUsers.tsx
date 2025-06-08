e3125634f4e3dd1bcd4c90714d3835ecc5b17929
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-08 15:07:16 +0300

    add translations to total users search bar

--- src/components/admin/routes/users/totalUsers/SearchTotalUsers.tsx
+++ src/components/admin/routes/users/totalUsers/SearchTotalUsers.tsx
@@ -4,14 +4,18 @@ import { memo } from "react";
 
 import SearchBar from "@/components/shared/SearchBar";
 import { useTotalUsers } from "@/contexts/TotalUsersContext";
+import { useTranslations } from "next-intl";
 
 const SearchTotalUsers = () => {
   const { searchQuery, setSearchQuery } = useTotalUsers();
+  const t = useTranslations();
 
   return (
     <div className="w-full">
       <SearchBar
-        placeholder="Search for users"
+        placeholder={t(
+          "routes.dashboard.routes.users.routes.totalUsers.components.SearchBar.placeholder"
+        )}
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
       />
