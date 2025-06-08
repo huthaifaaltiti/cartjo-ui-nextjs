b747f29ff878367632b74ee058da20059b2f751d
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-08 14:51:29 +0300

    add translations to users err page

--- src/app/[locale]/dashboard/users/error.tsx
+++ src/app/[locale]/dashboard/users/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ErrorPage from "@/components/shared/ErrorPage";
+import { useTranslations } from "next-intl";
 
 interface UsersErrorProps {
   error?: Error & { digest?: string };
@@ -8,11 +9,16 @@ interface UsersErrorProps {
 }
 
 export default function UsersError({ error, reset }: UsersErrorProps) {
+  const t = useTranslations();
+
   return (
     <ErrorPage
       error={error}
       reset={reset}
-      title={error?.message || "Failed to Load Users"}
+      title={
+        error?.message ||
+        t("routes.dashboard.routes.users.errors.failedLoadUsers")
+      }
       showReLogin={true}
     />
   );
