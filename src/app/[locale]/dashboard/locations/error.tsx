ebf6ab76160e9f8383ba153892faf37717466e61
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-08 14:53:20 +0300

    add locations error page

---
+++ src/app/[locale]/dashboard/locations/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import ErrorPage from "@/components/shared/ErrorPage";
+
+interface UsersErrorProps {
+  error?: Error & { digest?: string };
+  reset?: () => void;
+}
+
+export default function UsersError({ error, reset }: UsersErrorProps) {
+  const t = useTranslations();
+
+  return (
+    <ErrorPage
+      error={error}
+      reset={reset}
+      title={
+        error?.message ||
+        t("routes.dashboard.routes.locations.errors.failedLoadLocations")
+      }
+      showReLogin={true}
+    />
+  );
+}
