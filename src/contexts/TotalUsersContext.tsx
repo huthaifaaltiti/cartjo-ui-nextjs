54f945534f916fca8ea6f4d2ac72539ff6efa748
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-14 10:34:26 +0300

    total users context with actions

--- src/contexts/TotalUsersContext.tsx
+++ src/contexts/TotalUsersContext.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import { API_ENDPOINTS } from "@/lib/apiEndpoints";
+import { Locale } from "@/types/locale";
+import {
+  DeleteUserResponse,
+  SwitchUserActiveStatusResponse,
+  UnDeleteUserResponse,
+} from "@/types/totalUser";
 import { createContext, ReactNode, useContext, useState } from "react";
 
 type TotalUsersContextProps = {
   searchQuery: string;
   setSearchQuery: (searchQuery: string) => void;
   accessToken: string;
+  deleteUser: (token: string, userId: string) => Promise<DeleteUserResponse>;
+  unDeleteUser: (
+    token: string,
+    userId: string
+  ) => Promise<UnDeleteUserResponse>;
+  switchUserActiveStatus: (
+    token: string,
+    lang: Locale | string,
+    isActive: boolean,
+    userId: string
+  ) => Promise<SwitchUserActiveStatusResponse>;
 };
 
 type TotalUsersProviderProps = {
@@ -23,9 +41,86 @@ export const TotalUsersProvider = ({
 }: TotalUsersProviderProps) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
 
+  const deleteUser = async (
+    token: string,
+    userId: string
+  ): Promise<DeleteUserResponse> => {
+    const res = await fetch(
+      `${API_ENDPOINTS.DASHBOARD.USERS.DELETE_USER}/${userId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to delete user");
+    }
+
+    const resJson = await res.json();
+    return resJson;
+  };
+
+  const unDeleteUser = async (
+    token: string,
+    userId: string
+  ): Promise<DeleteUserResponse> => {
+    const res = await fetch(
+      `${API_ENDPOINTS.DASHBOARD.USERS.UNDELETE_USER}/${userId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to delete user");
+    }
+
+    const resJson = await res.json();
+    return resJson;
+  };
+
+  const switchUserActiveStatus = async (
+    token: string,
+    lang: Locale | string,
+    isActive: boolean,
+    userId: string
+  ) => {
+    const res = await fetch(
+      `${API_ENDPOINTS.DASHBOARD.USERS.SWITCH_USER_ACTIVE_STATUS}/${userId}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({ lang, isActive }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!res.ok) throw new Error("Could not switch user active status");
+
+    const resJson = await res.json();
+    return resJson;
+  };
+
   return (
     <TotalUsersContext.Provider
-      value={{ searchQuery, setSearchQuery, accessToken }}
+      value={{
+        searchQuery,
+        setSearchQuery,
+        accessToken,
+        deleteUser,
+        unDeleteUser,
+        switchUserActiveStatus,
+      }}
     >
       {children}
     </TotalUsersContext.Provider>
