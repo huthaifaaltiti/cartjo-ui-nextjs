c03592aea18c2d439a5ee0789ae09b9e530c79c1
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-14 12:38:21 +0300

    reuse access token getter in the page

--- src/app/[locale]/dashboard/users/total-users/page.tsx
+++ src/app/[locale]/dashboard/users/total-users/page.tsx
@@ -1,20 +1,11 @@
-import { getServerSession } from "next-auth";
-
-import { ExtendedSession } from "@/types/session";
-
-import { authOptions, CustomSession } from "@/lib/authOptions";
 import { PAGINATION_LIMITS } from "@/config/paginationConfig";
 import { fetchTotalUsers } from "@/hooks/react-query/useTotalUsersQuery";
 
 import TotalUsersPage from "@/components/admin/routes/users/totalUsers/TotalUsersPage";
+import { getAccessTokenFromServerSession } from "@/lib/serverSession";
 
 export default async function Page() {
-  const session = (await getServerSession(authOptions)) as ExtendedSession;
-  const accessToken = (session as CustomSession)?.accessToken;
-
-  if (!accessToken) {
-    throw new Error("Unauthorized token");
-  }
+  const accessToken = await getAccessTokenFromServerSession();
 
   const { users } = await fetchTotalUsers({
     token: accessToken,
