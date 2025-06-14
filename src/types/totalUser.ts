655eddd3a02ac77a2659fae9cb902d66adf3c5a3
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-14 10:34:52 +0300

    edit total users responses types

--- src/types/totalUser.ts
+++ src/types/totalUser.ts
@@ -1,9 +1,8 @@
-export type DeleteUserResponse = {
+export type CommonResponse = {
   isSuccess: boolean;
   message: string;
 };
 
-export type UnDeleteUserResponse = {
-  isSuccess: boolean;
-  message: string;
-};
+export type DeleteUserResponse = CommonResponse;
+export type UnDeleteUserResponse = CommonResponse;
+export type SwitchUserActiveStatusResponse = CommonResponse;
