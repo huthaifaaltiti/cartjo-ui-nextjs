2dd74ab201004353208cf4973f8414a05bd9d0e6
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-20 21:28:14 +0300

    allow localhost domain for images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains:
+      process.env.NODE_ENV === "development"
+        ? ["localhost"]
+        : ["your-live-domain.com"],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
