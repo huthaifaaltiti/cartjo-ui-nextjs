8ab48ff3c32f87851cd06f4c09ed8bcf5249b342
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-20 21:26:37 +0300

    add common types

---
+++ src/types/common.ts
@@ -0,0 +1,7 @@
+export type CommonResponse = {
+  isSuccess: boolean;
+  message: string;
+};
+
+export type DeletingResponse = CommonResponse;
+export type SwitchActiveStatusResponse = CommonResponse;
