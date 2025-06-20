4df87037ebe1200053357e33be4e53aeee86614b
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-06-20 21:29:19 +0300

    create loading categories page

--- src/app/[locale]/dashboard/categories/loading.tsx
+++ src/app/[locale]/dashboard/categories/loading.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from "@/components/shared/LoadingSpinner";
 
 export default function Loading() {
   return (
-    <div className="w-full min-h-[55vh] h-full flex items-center justify-center">
+    <div className="w-full h-full flex items-center justify-center">
       <LoadingSpinner size="lg" />
     </div>
   );
