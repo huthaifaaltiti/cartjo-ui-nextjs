4cddac95c2f2747006fd39e7032d39127b2228b7
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-07-11 20:24:21 +0300

    adding products page

--- src/app/[locale]/dashboard/products/page.tsx
+++ src/app/[locale]/dashboard/products/page.tsx
@@ -1,14 +1,19 @@
 import { Product } from "@/types/product.type";
+import { Category } from "@/types/category";
 
 import { getAccessTokenFromServerSession } from "@/lib/serverSession";
 import { PAGINATION_LIMITS } from "@/config/paginationConfig";
+
 import { fetchProducts } from "@/hooks/react-query/useProductsQuery";
+import { fetchCategories } from "@/hooks/react-query/useCategoriesQuery";
+
 import ProductsPage from "@/components/admin/routes/products/ProductsPage";
 
 export default async function Page() {
   const accessToken = await getAccessTokenFromServerSession();
 
   let products: Product[] = [];
+  let categories: Category[] = [];
 
   if (accessToken) {
     const prodsResp = await fetchProducts({
@@ -16,8 +21,20 @@ export default async function Page() {
       limit: PAGINATION_LIMITS.INITIAL_TOTAL_USERS_LIMIT,
     });
 
+    const catsResp = await fetchCategories({
+      token: accessToken,
+      limit: PAGINATION_LIMITS.INITIAL_TOTAL_USERS_LIMIT,
+    });
+
+    categories = catsResp?.categories || [];
     products = prodsResp?.data || [];
   }
 
-  return <ProductsPage initialProducts={products} token={accessToken} />;
+  return (
+    <ProductsPage
+      initialProducts={products}
+      initialCategories={categories}
+      token={accessToken}
+    />
+  );
 }
--- src/components/admin/routes/products/ProductsPage.tsx
+++ src/components/admin/routes/products/ProductsPage.tsx
@@ -1,19 +1,34 @@
 import { memo } from "react";
+import { BlocksIcon } from "lucide-react";
 
 import { Product } from "@/types/product.type";
 
 import { ProductsContextProvider } from "@/contexts/Products.context";
 import SearchProducts from "./SearchProducts";
 import ProductsList from "./ProductsList";
+import ModalCreateButton from "@/components/shared/ModalCreateButton";
+import CreateProductForm from "./CreateProductForm";
+import { Category } from "@/types/category";
 
 type ProductsPageProps = {
   initialProducts: Product[];
+  initialCategories: Category[];
   token: string;
 };
 
-const ProductsPage = ({ initialProducts, token }: ProductsPageProps) => {
+const ProductsPage = ({
+  initialProducts,
+  initialCategories,
+  token,
+}: ProductsPageProps) => {
   return (
     <ProductsContextProvider token={token}>
+      <ModalCreateButton
+        icon={<BlocksIcon />}
+        createTranslationKey="routes.dashboard.routes.products.createProduct.label"
+        ModalContent={<CreateProductForm categories={initialCategories} />}
+      />
+
       <SearchProducts />
 
       <div className="w-full mt-3">
