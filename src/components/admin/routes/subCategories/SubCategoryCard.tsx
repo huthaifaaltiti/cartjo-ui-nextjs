7bb4aabb754d34b15d4ea90d6bace00a37c6dd5d
huthaifa.titi <haltiti@arabica.org>
2025-07-24 11:17:16 +0300

    resize the subCategory card component

--- src/components/admin/routes/subCategories/SubCategoryCard.tsx
+++ src/components/admin/routes/subCategories/SubCategoryCard.tsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
 import Image from "next/image";
+import { useLocale, useTranslations } from "next-intl";
 
 import { DeletingResponse, SwitchActiveStatusResponse } from "@/types/common";
 
@@ -35,54 +36,56 @@ const SubCategoryCard = ({
   switchSubCategoryActiveStatus,
   queryKey,
 }: SubCategoryCardProps) => {
+  const t = useTranslations();
+  const isArabic = useLocale() === "ar";
+
   return (
     <div className="bg-white rounded-lg border border-gray-200 p-6 hover:shadow-md transition-shadow flex flex-col">
-      <div className="flex items-start justify-between mb-4">
-        <div className="flex items-center space-x-3">
-          <div className="w-10 h-10 rounded overflow-hidden bg-gray-100 border border-gray-200">
-            <Image
-              src={subCategory.image}
-              alt={subCategory.name.en}
-              width={40}
-              height={40}
-              className="object-cover w-full h-full"
-            />
-          </div>
-          <div>
-            <h3 className="text-sm font-semibold text-gray-900 capitalize">
-              {subCategory.name.ar}
-            </h3>
-            <p className="text-xs text-gray-600">{subCategory.name.en}</p>
-          </div>
-        </div>
-
-        <span className="w-auto flex items-center gap-1">
-          <span
-            className={`px-[5px] py-[0.8px] text-[10px] rounded-full ${
-              subCategory.isActive
-                ? "bg-green-100 text-green-800"
-                : "bg-red-100 text-red-800"
-            }`}
-          >
-            {subCategory.isActive ? "Active" : "Inactive"}
-          </span>
-
-          {subCategory.isDeleted && (
-            <span className="ml-2 px-[5px] py-[0.8px] text-[10px] rounded-full bg-gray-200 text-gray-700">
-              Deleted
-            </span>
-          )}
+      <div className="w-auto flex items-end justify-end gap-1 mb-1">
+        <span
+          className={`px-[5px] py-[0.8px] text-[10px] rounded-full ${
+            subCategory?.isActive
+              ? "bg-green-100 text-green-800"
+              : "bg-red-100 text-red-800"
+          }`}
+        >
+          {subCategory?.isActive
+            ? t("general.items.states.active")
+            : t("general.items.states.inactive")}
         </span>
+
+        {subCategory?.isDeleted && (
+          <span className="ml-2 px-[5px] py-[0.8px] text-[10px] rounded-full bg-gray-200 text-gray-700">
+            {t("general.items.states.deleted")}
+          </span>
+        )}
       </div>
 
-      <div className="text-xs text-gray-600 mb-4 space-y-1">
+      <div className="flex items-center space-x-3">
+        <div className="w-10 h-10 rounded overflow-hidden bg-gray-100 border border-gray-200">
+          <Image
+            src={subCategory?.mediaId?.supabaseBackupUrl || subCategory?.image}
+            alt={subCategory?.name.en}
+            width={40}
+            height={40}
+            className="object-cover w-full h-full"
+          />
+        </div>
+
+        <h3 className="text-sm font-semibold text-gray-900 capitalize">
+          {isArabic ? subCategory?.name.ar : subCategory?.name.en}
+        </h3>
+      </div>
+
+      <div className="text-xs text-gray-600 mb-4 my-1">
         <p>
-          Created: {new Date(subCategory.createdAt).toLocaleDateString("en-US")}
+          {t("general.others.created")}:{" "}
+          {new Date(subCategory?.createdAt).toLocaleDateString("en-US")}
         </p>
-        {subCategory.updatedAt && (
+        {subCategory?.updatedAt && (
           <p>
-            Updated:{" "}
-            {new Date(subCategory.updatedAt).toLocaleDateString("en-US")}
+            {t("general.others.updated")}:{" "}
+            {new Date(subCategory?.updatedAt).toLocaleDateString("en-US")}
           </p>
         )}
       </div>
