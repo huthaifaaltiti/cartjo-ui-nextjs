15353e79d28c280e611238f564b108a1f503d2cd
huthaifa.titi <haltiti@arabica.org>
2025-08-13 09:28:34 +0300

    fix(token): deal with active showcases as a public route

--- src/components/user/home/HomeShowcase.tsx
+++ src/components/user/home/HomeShowcase.tsx
@@ -11,7 +11,7 @@ const HomeShowcase = async () => {
   return (
     <div className="w-full min-h-screen h-full bg-gradient-to-b from-gray-100 to-white-50">
       <MaxWidthWrapper className="w-full py-14">
-        <ShowcasesContextProvider accessToken={token}>
+        <ShowcasesContextProvider accessToken={token || ""}>
           <HomeShowcaseContent />
         </ShowcasesContextProvider>
       </MaxWidthWrapper>
