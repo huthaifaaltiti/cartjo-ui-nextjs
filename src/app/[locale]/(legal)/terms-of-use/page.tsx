9d8f3c4362adac5389349d8b06dc27bbb3bf03ea
huthaifa.titi <haltiti@arabica.org>
2025-10-13 11:17:18 +0300

    adding terms of use

--- src/app/[locale]/(legal)/terms-of-use/page.tsx
+++ src/app/[locale]/(legal)/terms-of-use/page.tsx
@@ -1,27 +1,27 @@
 import LegalPageRenderer from "@/components/user/legal/LegalPageRenderer";
 
-type PrivacyPolicyPageProps = {
+type TermsOfUsePageProps = {
   params: {
     locale: string;
   };
 };
 
-const PrivacyPolicyPage = ({ params }: PrivacyPolicyPageProps) => {
+const TermsOfUsePage = ({ params }: TermsOfUsePageProps) => {
   return (
     <LegalPageRenderer
       locale={params.locale}
-      folderName="policies"
-      fileName="privacy-policy"
-      titleKey="routes.privacyPolicy.title"
-      dateKey="routes.privacyPolicy.lastUpdated"
+      folderName="terms-of-use"
+      fileName="terms-of-use"
+      titleKey="routes.termsOfUse.title"
+      dateKey="routes.termsOfUse.lastUpdated"
     />
   );
 };
 
-export default PrivacyPolicyPage;
+export default TermsOfUsePage;
 
 export const metadata = {
-  title: "Privacy Policy | CartJO",
+  title: "Terms of Use | CartJO",
   description:
     "Learn how CartJO protects your personal data and privacy while using our e-commerce platform.",
 };
