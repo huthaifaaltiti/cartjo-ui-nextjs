8c641f9d61d4743a534c576b3379a17538ad8b14
huthaifa.titi <haltiti@arabica.org>
2025-10-14 15:27:07 +0300

    adding home link at top bar

---
+++ src/components/HomeLink.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { memo } from "react";
+import { House } from "lucide-react";
+import Link from "next/link";
+import { useGeneralContext } from "@/contexts/General.context";
+
+const HomeLink: React.FC = () => {
+  const { isArabic } = useGeneralContext();
+
+  return (
+    <div
+      className={`flex items-center ${
+        isArabic
+          ? "border-l border-gray-200 pl-3"
+          : "border-r border-gray-200 pr-3"
+      }`}
+    >
+      <Link href="/" aria-label="Go to homepage">
+        <House className="w-6 h-6 text-primary-500 hover:text-primary-600 cursor-pointer transition-colors duration-200" />
+      </Link>
+    </div>
+  );
+};
+
+export default memo(HomeLink);
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,8 +1,8 @@
 import { memo } from "react";
 import { useTranslations } from "next-intl";
-
 import MaxWidthWrapper from "./shared/MaxWidthWrapper";
 import LanguageSelector from "./LanguageSelector";
+import HomeLink from "./HomeLink";
 
 const TopBar: React.FC = () => {
   const t = useTranslations("components.TopBar");
@@ -11,14 +11,18 @@ const TopBar: React.FC = () => {
     <div className="w-full py-3 border-b border-grey-50/15">
       <MaxWidthWrapper>
         <div className="w-full flex items-center justify-between">
-          <p className="text-sm text-text-primary-100">
-            {t("deliver")}
-            <span className="mx-1 text-text-tertiary-400 font-bold">
-              <span>{t("fromTime")}</span>
-              <span className="mx-1">{t("to")}</span>
-              <span>{t("toTime")}</span>
-            </span>
-          </p>
+          <div className="w-auto flex items-center gap-3">
+            <HomeLink />
+
+            <p className="text-sm text-text-primary-100">
+              {t("deliver")}
+              <span className="mx-1 text-text-tertiary-400 font-bold">
+                <span>{t("fromTime")}</span>
+                <span className="mx-1">{t("to")}</span>
+                <span>{t("toTime")}</span>
+              </span>
+            </p>
+          </div>
 
           <LanguageSelector />
         </div>
