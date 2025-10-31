3d9846ffd92d98296904bd608dd019c352e864b4
huthaifa.titi <haltiti@arabica.org>
2025-10-31 15:54:36 +0300

    set cart icon

--- src/components/UserWishlistAndCart.tsx
+++ src/components/UserWishlistAndCart.tsx
@@ -1,13 +1,19 @@
-import { Heart } from "lucide-react";
+import { Heart, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import { memo } from "react";
 
 const UserWishlistAndCart = () => {
   return (
-    <div className="w-auto flex items-center gap-1">
+    <div className="w-auto flex items-center gap-5">
       <Link href={"/wishlist"}>
         <span className="cursor-pointer">
           <Heart className="w-5 h-5 text-primary-500 hover:fill-primary-500 transition-all" />
+        </span>
+      </Link>
+
+      <Link href={"/cart"}>
+        <span className="cursor-pointer">
+          <ShoppingCart className="w-5 h-5 text-primary-500 hover:fill-primary-500 transition-all" />
         </span>
       </Link>
     </div>
