f4833755ec5058d3aa160890f0be7fc9881b8eff
huthaifa.titi <haltiti@arabica.org>
2025-12-02 13:20:15 +0300

    add sizing classes

--- src/components/user/general/footer/top/FooterTop.tsx
+++ src/components/user/general/footer/top/FooterTop.tsx
@@ -5,21 +5,23 @@ const FooterTop = () => {
   const t = useTranslations();
 
   return (
-    <div className="w-full flex justify-between gap-5 border-b border-gray-100 py-5">
-      <div className="w-auto">
-        <p className="text-md font-bold text-text-primary-300">
+    <div className="w-full flex flex-col lg:flex-row justify-between gap-5 border-b border-gray-100 py-5 px-4 sm:px-6 lg:px-8">
+      {/* Left section */}
+      <div className="w-full lg:w-auto">
+        <p className="text-sm sm:text-md lg:text-lg font-bold text-text-primary-300">
           {t("components.Footer.components.FooterTop.title")}
         </p>
-        <p className="text-xs text-text-primary-100">
+        <p className="text-xs sm:text-sm lg:text-md text-text-primary-100">
           {t("components.Footer.components.FooterTop.desc")}
         </p>
       </div>
 
-      <div className="w-auto flex flex-col gap-1">
-        <span className="text-xs font-bold text-text-primary-100 uppercase">
+      {/* Right section */}
+      <div className="w-full lg:w-auto flex flex-col gap-1 mt-4 lg:mt-0">
+        <span className="text-xs sm:text-sm lg:text-md font-bold text-text-primary-100 uppercase">
           {t("components.Footer.components.FooterTop.emailSupport")}
         </span>
-        <span className="text-md font-bold text-text-primary-300">
+        <span className="text-sm sm:text-md lg:text-lg font-bold text-text-primary-300">
           {t("components.Footer.components.FooterTop.email")}
         </span>
       </div>
