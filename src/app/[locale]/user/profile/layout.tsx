d9149902b9b170a569d49443e4ebad803a80b7c0
huthaifa.titi <haltiti@arabica.org>
2025-12-11 10:26:23 +0300

    add promise type

--- src/app/[locale]/user/profile/layout.tsx
+++ src/app/[locale]/user/profile/layout.tsx
@@ -1,9 +1,12 @@
 export default async function UserProfilePageLayout({
-  children,
-  // params,
+  children
 }: {
   children: React.ReactNode;
-  params?: { slug?: string[] };
+  params?: Promise<{ slug?: string[] }>;
 }) {
+  // If you need to use params, await it here:
+  // const resolvedParams = params ? await params : undefined;
+  // const slug = resolvedParams?.slug;
+
   return <div className="fw-full h-full">{children}</div>;
 }
