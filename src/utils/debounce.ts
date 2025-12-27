28d5138bb889d16938e7835778cc6b9372a78142
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2025-12-27 15:33:37 +0300

    Build fixes (#31)
    * set direction to the stepper's steps
    
    * send locale with apis and fix ui directions
    
    * fix spacing
    
    * delete import spacing
    
    * erase spacing and require auth on server comp
    
    * update footer placement for some routes
    
    * fix token types in users/dashboard
    
    * fix token types in categories/dashboard
    
    * fix token types in categories/dashboard
    
    * fix token types in sub-categories/dashboard
    
    * add token type
    
    * fix token types in products/dashboard
    
    * remove media and locations from dashboard navigation
    
    * fix token types in logos/dashboard
    
    * fix token types in banners/dashboard
    
    * add require auth func to the type-hint-configs
    
    * fix token types in showcases/dashboard
    
    * add require auth func to the orders/dashboard
    
    * add logout btn to the dashboard
    
    * replace category page product card
    
    * replace sub-category page product card
    
    * fix build issues
    
    * fix build issues
    
    * fix build issues
    
    * reset cart and order after place an order
    
    * add not-found app page and set orders route
    
    * delete footer from user route
    
    * feat: create debounce function
    
    * feat: setting up user profile orders
    
    * add eslint rule
    
    * add dashboard order card statuses
    
    * feat: add orders filter by delivery status
    
    * feat: ad change pay/del statuses in orders
    
    * add translations
    
    * hide user profile actions
    
    * feat: implement orders returns section
    
    * fix no orders data logic
    
    * fix build issues 27-12-25 #1
    
    * fix build issues 27-12-25 #2
    
    * fix build issues 27-12-25 #3
    
    * fix build issues 27-12-25 #4
    
    ---------
    
    Co-authored-by: huthaifa.titi <haltiti@arabica.org>

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,13 +1,28 @@
-export function debounce<T extends (...args: unknown[]) => void>(
-  fn: T,
+// export function debounce<T extends (...args: unknown[]) => void>(
+//   fn: T,
+//   delay: number
+// ) {
+//   let timer: ReturnType<typeof setTimeout>;
+
+//   return (...args: Parameters<T>) => {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       fn(...args);
+//     }, delay);
+//   };
+// }
+
+export function debounce<Args extends unknown[]>(
+  fn: (...args: Args) => void,
   delay: number
 ) {
   let timer: ReturnType<typeof setTimeout>;
 
-  return (...args: Parameters<T>) => {
+  return (...args: Args) => {
     clearTimeout(timer);
     timer = setTimeout(() => {
       fn(...args);
     }, delay);
   };
 }
+
