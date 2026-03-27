624879f1e903ff58ceb7fd2570eb49a78092bb88
huthaifa.titi <haltiti@arabica.org>
2026-03-27 15:29:06 +0300

    fix: category slice error return type

--- src/redux/slices/category/index.ts
+++ src/redux/slices/category/index.ts
@@ -2,10 +2,11 @@ import { SLICES_INFO } from "@/redux/slices-info";
 import { Category } from "@/types/category.type";
 import { createSlice } from "@reduxjs/toolkit";
 import { getCategories } from "./actions";
+import { BaseResponse } from "@/types/service-response.type";
 
 interface CategoryState {
   loading: boolean;
-  err: Error | null;
+  err: BaseResponse | null;
   categories: Category[];
 }
 
@@ -20,22 +21,25 @@ const categorySlice = createSlice({
   initialState,
   reducers: {
     setCategories: (state, action) => {
-        console.log({action})
+      console.log({ action });
       state.categories = action.payload;
       state.loading = false;
       state.err = null;
     },
   },
   extraReducers: (builder) => {
+    // Loading
     builder.addCase(getCategories.pending, (state) => {
       state.loading = true;
     });
 
+    // Fulfilled
     builder.addCase(getCategories.fulfilled, (state, { payload }) => {
       state.loading = false;
       state.categories = payload.data ?? [];
     });
 
+    // Rejected
     builder.addCase(getCategories.rejected, (state, { payload }) => {
       state.loading = false;
       state.err = payload ?? null;
