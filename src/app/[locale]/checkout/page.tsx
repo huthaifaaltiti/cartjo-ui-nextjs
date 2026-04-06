ccbb8314c2ae37cb465033ad4f027a1618968b87
huthaifa.titi <haltiti@arabica.org>
2026-04-06 15:24:44 +0300

    fix import

--- src/app/[locale]/checkout/page.tsx
+++ src/app/[locale]/checkout/page.tsx
@@ -5,9 +5,9 @@ import { getAccessTokenFromServerSession } from "@/lib/serverSession";
 import { getQueryClient } from "@/utils/queryUtils";
 import { DataResponse } from "@/types/service-response.type";
 import { Cart } from "@/types/cart.type";
-import { getCartQueryOptions } from "@/utils/queryOptions";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { WishlistContextProvider } from "@/contexts/Wishlist.context";
+import { getCartQueryOptions } from "@/hooks/react-query/useCartQuery";
 
 const PaymentCheckoutPage: React.FC = async () => {
   const token = await getAccessTokenFromServerSession();
