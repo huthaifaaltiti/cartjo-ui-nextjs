7fc804ecf1f3eed74c843ee1d671e332fe75b2a0
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2026-07-11 12:58:22 +0300

    refactor: checking access permissions at layout stage for banners

--- src/app/[locale]/dashboard/banners/layout.tsx
+++ src/app/[locale]/dashboard/banners/layout.tsx
@@ -1,10 +1,27 @@
 import BannersPageHeader from "@/components/admin/routes/banners/BannersPageHeader";
+import { DashboardModule } from "@/enums/dashboard-module.enum";
+import { getModulePathname, requireRoutePermissions } from "@/lib/access-route";
+import { getSession } from "@/lib/session.server";
+import { getAccessToken } from "@/lib/tokens.server";
+import { requireAuth } from "@/utils/authRedirect";
 
 export default async function PageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getSession();
+  const token = await getAccessToken();
+
+  const pathname = getModulePathname(DashboardModule.BANNERS);
+
+  requireAuth(token);
+  requireRoutePermissions({
+    session,
+    pathname: pathname ?? "/dashboard/banners",
+    redirectTo: "/dashboard",
+  });
+
   return (
     <div className="w-full h-full">
       <BannersPageHeader />
