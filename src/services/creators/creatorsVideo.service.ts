cacc0aedec610271b79435dfcd4a5d9ae65f3692
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2026-09-09 20:48:18 +0300

    refactor: set create/update createsVideo

--- src/services/creators/creatorsVideo.service.ts
+++ src/services/creators/creatorsVideo.service.ts
@@ -5,6 +5,7 @@ import { Locale as LocaleEnum } from "@/enums/locale.enum";
 import { PAGINATION_LIMITS } from "@/config/paginationConfig";
 import { CreatorsVideo } from "@/types/creators/creatorsVideo";
 import { CreatorsVideoType } from "@/enums/creatorsVideoType.enum";
+import { authFetcher } from "@/utils/authFetcher";
 
 interface FetchActiveCreatorsVideosParams {
   lang?: Locale | string;
@@ -51,4 +52,67 @@ export const fetchCreatorsVideos = async ({
   if (type) url.searchParams.append("type", type.toLocaleLowerCase());
 
   return fetcher(url.toString());
+};
+
+export interface CreateCreatorsVideoPayload {
+  title_ar: string;
+  title_en: string;
+  type: string;
+  video: File;
+  lang?: string;
+}
+
+export const createCreatorsVideo = async (
+  payload: CreateCreatorsVideoPayload,
+  fetcher: (
+    path: string,
+    options?: RequestInit,
+  ) => Promise<DataResponse<CreatorsVideo>> = authFetcher,
+): Promise<DataResponse<CreatorsVideo>> => {
+  const formData = new FormData();
+  formData.append("title_ar", payload.title_ar);
+  formData.append("title_en", payload.title_en);
+  formData.append("type", payload.type?.toLowerCase());
+  formData.append("video", payload.video);
+
+  const lang = payload.lang ? `?lang=${payload.lang}` : "";
+
+  return fetcher(`${API_ENDPOINTS.DASHBOARD.CREATORS_VIDEOS.CREATE}${lang}`, {
+    method: "POST",
+    body: formData,
+  });
+};
+
+export interface UpdateCreatorsVideoPayload {
+  id: string;
+  title_ar?: string;
+  title_en?: string;
+  type?: string;
+  video?: File | null;
+  lang?: string;
+}
+
+export const updateCreatorsVideo = async (
+  payload: UpdateCreatorsVideoPayload,
+  fetcher: (
+    path: string,
+    options?: RequestInit,
+  ) => Promise<DataResponse<CreatorsVideo>> = authFetcher,
+): Promise<DataResponse<CreatorsVideo>> => {
+  const formData = new FormData();
+  formData.append("id", payload.id);
+  if (payload.title_ar) formData.append("title_ar", payload.title_ar);
+  if (payload.title_en) formData.append("title_en", payload.title_en);
+  if (payload.type) formData.append("type", payload.type?.toLowerCase());
+  if (payload.video) formData.append("video", payload.video);
+
+  const lang = payload.lang ? `?lang=${payload.lang}` : "";
+
+  return fetcher(
+    `${API_ENDPOINTS.DASHBOARD.CREATORS_VIDEOS.EDIT}/${payload.id}${lang}`,
+    {
+      method: "PUT",
+      body: formData,
+    },
+  );
 };
